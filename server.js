76364ccde3743ff1f4d7d6685ff8fd9d6970ec4b
amit5511 <amit.verma@pinnacleworks.net>
2023-09-09 20:26:35 +0530

    cors

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ require('dotenv').config();
 const app = require('./app');
 const dbconnection = require('./db/dbconnection');
 
-const cors = require('cors')
-
-  
-app.use(cors({
-    origin: 'https://brown-salesmen-ztxbw.pwskills.app:3001'
-}));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://brown-salesmen-ztxbw.pwskills.app:3001');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 //connecing mongodb
 const URL = process.env.MongoURL
 dbconnection(URL)
